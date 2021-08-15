17c2979c427a44575bfe1c34e909e7a60f9a5e8b
Talal-94 <mito.gldr9@gmail.com>
2021-08-15 19:16:24 +0300

    5th

--- app.js
+++ app.js
@@ -2,11 +2,8 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-
 //event listeners
 todoButton.addEventListener('click', addTodo)
-
-
 //functions
 function addTodo(event) {
     //prevent form from submitting 
